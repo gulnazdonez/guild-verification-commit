46a227dc2cc2bc6d86086a5c7438399de9292ed9
gulnazdonez <127683595+gulnazdonez@users.noreply.github.com>
2026-03-05 20:11:40 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:11:10.708Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:11:39.511Z
+Commit: Fix CSS styling bug on mobile devices
