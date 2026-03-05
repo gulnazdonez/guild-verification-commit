b6a9016e83c45a9306edd81d0f1612026bc75c9c
gulnazdonez <127683595+gulnazdonez@users.noreply.github.com>
2026-03-05 20:34:11 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:11:29.599Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T17:34:11.088Z
+Commit: Fix memory leak in event handlers
