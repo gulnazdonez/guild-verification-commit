9d4a87177bc7b877f58d40a57b3e02d809ca493f
gulnazdonez <127683595+gulnazdonez@users.noreply.github.com>
2026-03-05 20:11:30 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:11:27.813Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T17:11:29.599Z
+Commit: Fix CSS styling bug on mobile devices
