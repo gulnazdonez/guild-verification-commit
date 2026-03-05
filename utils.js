33c3669e53784582270d46fabdc401408c8edb77
gulnazdonez <127683595+gulnazdonez@users.noreply.github.com>
2026-03-05 20:10:37 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:09:10.190Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T17:10:36.820Z
+Commit: Patch API endpoint error handling
