4d34a82a20428e891a8bdecc819fbebc0cf2fe99
gulnazdonez <127683595+gulnazdonez@users.noreply.github.com>
2026-03-05 20:35:06 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:34:22.458Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:35:06.319Z
+Commit: Resolve null pointer exception in user service
