88b1fc73f40b98388f944f5e6b3e5a1fa029923f
gulnazdonez <127683595+gulnazdonez@users.noreply.github.com>
2026-03-06 22:42:08 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:32:26.312Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T19:42:08.332Z
+Commit: Fix CSS styling bug on mobile devices
