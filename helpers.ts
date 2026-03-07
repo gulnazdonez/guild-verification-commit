d0ba57c2decc229861bdc950ea603c61fe902760
gulnazdonez <127683595+gulnazdonez@users.noreply.github.com>
2026-03-07 21:50:18 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:42:16.560Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-07T18:50:17.952Z
+Commit: Resolve null pointer exception in user service
