d83bee1cc0b3f0dbe97c05da950c18f7ee57e525
gulnazdonez <127683595+gulnazdonez@users.noreply.github.com>
2026-03-10 22:30:51 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T19:30:42.167Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-10T19:30:50.855Z
+Commit: Fix memory leak in event handlers
